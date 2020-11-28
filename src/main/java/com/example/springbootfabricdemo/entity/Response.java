273542df863c1d461e415bd3632681fd8218fb97
xusysh <xusysh@126.com>
2020-11-28 23:44:30 +0800

    1128

--- src/main/java/com/example/springbootfabricdemo/entity/Response.java
+++ src/main/java/com/example/springbootfabricdemo/entity/Response.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class Response<T> {
 
     private int status;
@@ -12,10 +13,6 @@ public class Response<T> {
     private String msg;
 
     T data;
-
-    public Response(int status, String msg, T data) {
-        return ;
-    }
 
     public static <T> Response<T> newSuccInstance(T data) {
         return new Response(200, "成功", data);
