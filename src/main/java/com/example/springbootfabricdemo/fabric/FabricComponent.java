0262a00bbb52405dab731b8d58b861d9bed736a0
xusysh <xusysh@126.com>
2020-11-29 22:38:30 +0800

    1129

--- src/main/java/com/example/springbootfabricdemo/fabric/FabricComponent.java
+++ src/main/java/com/example/springbootfabricdemo/fabric/FabricComponent.java
@@ -2,7 +2,25 @@ package com.example.springbootfabricdemo.fabric;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class FabricComponent {
+
+    public void enrollAdmin(String userName, String passwd) {
+
+    }
+
+    public void registerUser(String userName, String passwd) {
+
+    }
+
+    public void invokeQuery(String apiName, List<Object> args) {
+
+    }
+
+    public void invokeTx(String apiName, List<Object> args) {
+
+    }
 
 }
