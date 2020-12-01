930827b602704d2fb362422e29e60bd69c9dcaca
gjy gjy <xusysh@126.com>
2020-12-01 10:36:14 +0800

    1201

---
+++ src/main/java/com/example/springbootfabricdemo/dto/fabric/req/FinLocQuery.java
@@ -0,0 +1,13 @@
+package com.example.springbootfabricdemo.dto.fabric.req;
+
+import java.util.Date;
+
+public class FinLocQuery {
+
+    String userId;
+
+    Date startTime;
+
+    Date endTime;
+
+}
---
+++ src/main/java/com/example/springbootfabricdemo/dto/fabric/req/FinQuery.java
@@ -0,0 +1,13 @@
+package com.example.springbootfabricdemo.dto.fabric.req;
+
+import java.util.Date;
+
+public class FinQuery {
+
+    String userId;
+
+    String startTime;
+
+    String endTime;
+
+}
