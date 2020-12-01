907dc4f42b625fdfc5326a74538b458148ad4c0b
gjy gjy <xusysh@126.com>
2020-12-01 19:58:48 +0800

    1201

--- src/main/java/com/example/springbootfabricdemo/fabric/FabricComponent.java
+++ src/main/java/com/example/springbootfabricdemo/fabric/FabricComponent.java
@@ -1,25 +1,68 @@
 package com.example.springbootfabricdemo.fabric;
 
+import com.example.springbootfabricdemo.config.FabricConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.net.MalformedURLException;
 import java.util.List;
+
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallet.Identity;
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import org.hyperledger.fabric.sdk.security.CryptoSuiteFactory;
+import org.hyperledger.fabric_ca.sdk.EnrollmentRequest;
+import org.hyperledger.fabric_ca.sdk.HFCAClient;
 
 @Component
 public class FabricComponent {
 
-    public void enrollAdmin(String userName, String passwd) {
+    @Autowired
+    FabricConfig fabricConfig;
 
+    public void enrollAdmin(String userName, String passwd) throws Exception {
+        // Create a CA client for interacting with the CA.
+        Properties props = new Properties();
+        props.put("pemFile",
+                "../../crypto-config/peerOrganizations/org1.ccb.com/ca/ca.org1.ccb.com-cert.pem");
+        props.put("allowAllHostNames", "true");
+        HFCAClient caClient = HFCAClient.createNewInstance("https://localhost:7054", props);
+        CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
+        caClient.setCryptoSuite(cryptoSuite);
+
+        // Create a wallet for managing identities
+        Wallet wallet = Wallet.createFileSystemWallet(Paths.get("wallet"));
+
+        // Check to see if we've already enrolled the admin user.
+        boolean adminExists = wallet.exists("admin");
+        if (adminExists) {
+            System.out.println("An identity for the admin user \"admin\" already exists in the wallet");
+            return;
+        }
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        final EnrollmentRequest enrollmentRequestTLS = new EnrollmentRequest();
+        enrollmentRequestTLS.addHost("localhost");
+        enrollmentRequestTLS.setProfile("tls");
+        Enrollment enrollment = caClient.enroll("admin", "adminpw", enrollmentRequestTLS);
+        Identity user = Identity.createIdentity("Org1MSP", enrollment.getCert(), enrollment.getKey());
+        wallet.put("admin", user);
+        System.out.println("Successfully enrolled user \"admin\" and imported it into the wallet");
     }
 
     public void registerUser(String userName, String passwd) {
 
     }
 
-    public void invokeQuery(String apiName, List<Object> args) {
+    public void invokeQuery(String apiName, Object params) {
 
     }
 
-    public void invokeTx(String apiName, List<Object> args) {
+    public void invokeTx(String apiName, Object params) {
 
     }
 
