0c004dcb15262fc774ffa1b283fb62ec59732b16
gjy gjy <xusysh@126.com>
2020-12-02 14:13:28 +0800

    1202

--- src/main/java/com/example/springbootfabricdemo/config/FabricConfig.java
+++ src/main/java/com/example/springbootfabricdemo/config/FabricConfig.java
@@ -1,34 +1,44 @@
 package com.example.springbootfabricdemo.config;
 
 import lombok.Getter;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.User;
+import org.hyperledger.fabric.gateway.Wallet.Identity;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
 import org.hyperledger.fabric.sdk.security.CryptoSuiteFactory;
 import org.hyperledger.fabric_ca.sdk.HFCAClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.security.PrivateKey;
 import java.util.Properties;
+import java.util.Set;
 
 @Configuration
 @Getter
+@ConfigurationProperties(prefix = "mytest.user")
 public class FabricConfig {
 
     String configPath;
 
-    String walletPath;
+    String walletPath = "wallet";
 
-    String orgName;
+    String orgName = "Org1";
 
-    String channelName;
+    String channelName = "mychannel";
 
-    String chaincodeName;
+    String chaincodeName = "mycc";
 
     String caName = "ca.org1.ccb.com";
 
     String caUrl = "https://localhost:7054";
-
-    HFCAClient hfcaClient = null;
 
     @Bean
     public HFCAClient HFCAClient() throws Exception {
@@ -41,5 +51,12 @@ public class FabricConfig {
         caClient.setCryptoSuite(cryptoSuite);
         return caClient;
     }
+
+    public Wallet getWallet() throws IOException {
+        Wallet wallet = Wallet.createFileSystemWallet(Paths.get(walletPath));
+        return wallet;
+    }
+
+
 
 }
--- src/test/java/com/example/springbootfabricdemo/SpringbootFabricDemoApplicationTests.java
+++ src/test/java/com/example/springbootfabricdemo/SpringbootFabricDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.springbootfabricdemo;
 
+import com.example.springbootfabricdemo.dto.fabric.req.FinLocQuery;
+import org.apache.commons.beanutils.BeanUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.Map;
 
 @SpringBootTest
 class SpringbootFabricDemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        Map<String,String> emm = BeanUtils.describe(new FinLocQuery("asd",new Date(),new Date()));
+        String[] emm2 = emm.values().toArray(new String[0]);
+        System.out.println(emm2);
     }
 
 }
